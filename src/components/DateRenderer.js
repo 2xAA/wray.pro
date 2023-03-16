86e9fb3304264d5caa025c5167dc88a909c732ec
Sam Wray <sam@wray.pro>
2023-03-16 22:34:22 +0000

    fix: update default youtube args (#98)
    also satisfy linter

--- src/components/DateRenderer.js
+++ src/components/DateRenderer.js
@@ -3,7 +3,7 @@ import React from "react";
 export const DateRenderer = ({ date, includeTime }) => {
   const event = React.useMemo(() => {
     return new Date(date);
-  });
+  }, [event]);
 
   const dateString = React.useMemo(() => {
     const options = {
@@ -14,11 +14,11 @@ export const DateRenderer = ({ date, includeTime }) => {
     };
 
     return event.toLocaleDateString("en-GB", options);
-  });
+  }, [event]);
 
   const timeString = React.useMemo(() => {
     return event.toLocaleTimeString("en-GB");
-  });
+  }, [event]);
 
   return (
     <>
