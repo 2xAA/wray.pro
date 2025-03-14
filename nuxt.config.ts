e0374df369755cdd8a1ed3d01fa764fac3f8141b
Sam Wray <sam@wray.pro>
2025-03-14 16:58:49 +0000

    fix: prerender pages (#147)
    * Update style.css (#145)
    
    * Update [uid].vue (#144)
    
    * fix: prerender pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,62 +1,92 @@
-import { apiEndpoint, repositoryName } from "./slicemachine.config.json";
+import { createClient } from "@prismicio/client";
+import { repositoryName } from "./slicemachine.config.json";
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
-  devtools: { enabled: true },
-  ssr: false,
-  modules: [
-    "@nuxtjs/prismic",
-    "nuxt-svgo-loader",
-    "@nuxt/scripts",
-    "@nuxt/eslint",
-  ],
+async function generatePrerenderRoutes() {
+  const prismic = createClient(repositoryName);
 
-  app: {
-    head: {
-      charset: "utf-8",
-      viewport: "width=device-width, initial-scale=1",
-      htmlAttrs: {
-        lang: "en",
+  // Fetch dynamic content from Prismic
+  const [workPages, blogPosts] = await Promise.all([
+    prismic.getAllByType("work"),
+    prismic.getAllByType("blog_post"),
+  ]);
+
+  return [
+    // Work dynamic routes
+    ...workPages.map((page) => `/work/${page.uid}`),
+
+    // Blog post dynamic routes
+    ...blogPosts.map((post) => `/journal/${post.uid}`),
+  ];
+}
+
+export default (async () => {
+  const prerenderRoutes = await generatePrerenderRoutes();
+
+  return defineNuxtConfig({
+    compatibilityDate: "2024-04-03",
+    devtools: { enabled: true },
+    ssr: true,
+    modules: [
+      "@nuxtjs/prismic",
+      "nuxt-svgo-loader",
+      "@nuxt/scripts",
+      "@nuxt/eslint",
+    ],
+
+    app: {
+      head: {
+        charset: "utf-8",
+        viewport: "width=device-width, initial-scale=1",
+        htmlAttrs: {
+          lang: "en",
+        },
+        link: [{ rel: "icon", type: "image/png", href: "/favicon.png" }],
       },
-      link: [{ rel: "icon", type: "image/png", href: "/favicon.png" }],
     },
-  },
 
-  vue: {
-    compilerOptions: {
-      isCustomElement: (tag) => {
-        return ["r-grid", "r-cell"].indexOf(tag) > -1;
+    vue: {
+      compilerOptions: {
+        isCustomElement: (tag) => {
+          return ["r-grid", "r-cell"].indexOf(tag) > -1;
+        },
       },
     },
-  },
 
-  prismic: {
-    endpoint: apiEndpoint || repositoryName,
-
-    clientConfig: {
-      routes: [
-        // Resolves the Homepage document to "/"
-        {
-          type: "home",
-          path: "/",
-        },
-        {
-          type: "work",
-          path: "/work/:uid",
-        },
-        {
-          type: "about_new",
-          path: "/about/",
-        },
-        {
-          type: "blog_home",
-          path: "/journal/",
-        },
-        {
-          type: "blog_post",
-          path: "/journal/:uid",
-        },
-      ],
+    nitro: {
+      prerender: {
+        routes: prerenderRoutes,
+        crawlLinks: true, // Auto-discover links
+      },
     },
-  },
-});
+
+    prismic: {
+      endpoint: repositoryName,
+
+      clientConfig: {
+        routes: [
+          // Resolves the Homepage document to "/"
+          {
+            type: "home",
+            path: "/",
+          },
+          {
+            type: "work",
+            path: "/work/:uid",
+          },
+          {
+            type: "about_new",
+            path: "/about/",
+          },
+          {
+            type: "blog_home",
+            path: "/journal/",
+          },
+          {
+            type: "blog_post",
+            path: "/journal/:uid",
+          },
+        ],
+      },
+    },
+  });
+})();
